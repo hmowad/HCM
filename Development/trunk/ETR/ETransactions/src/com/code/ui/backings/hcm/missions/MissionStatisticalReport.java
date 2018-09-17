3a03a2ba191775eed22c418b49a19590b35acb3f
aabdelgayed <aabdelgayed@c23e29db-d35b-224f-a253-3de55204965d>
2018-09-17 13:19:47 +0000

    CR-618

--- Development/trunk/ETR/ETransactions/src/com/code/ui/backings/hcm/missions/MissionStatisticalReport.java
+++ Development/trunk/ETR/ETransactions/src/com/code/ui/backings/hcm/missions/MissionStatisticalReport.java
@@ -8,8 +8,11 @@ import javax.faces.bean.ViewScoped;
 
 import com.code.dal.orm.hcm.organization.Region;
 import com.code.enums.FlagsEnum;
+import com.code.enums.MenuActionsEnum;
+import com.code.enums.MenuCodesEnum;
 import com.code.exceptions.BusinessException;
 import com.code.services.hcm.MissionsService;
+import com.code.services.security.SecurityService;
 import com.code.services.util.CommonService;
 import com.code.services.util.HijriDateService;
 import com.code.ui.backings.base.BaseBacking;
@@ -18,11 +21,21 @@ import com.code.ui.backings.base.BaseBacking;
 @ViewScoped
 public class MissionStatisticalReport extends BaseBacking {
 
+    private int reportType = 1;
+    private Long empId;
+    private String empFullName;
+    private Long orgUnitId;
+    private String orgUnitFullName;
+    private String orgUnitHKey;
+
     private List<Region> regionsList;
     private int category;
     private long regionId;
     private Date toDate;
     private Date fromDate;
+
+    private Boolean isManager;
+    private Boolean isAdmin;
 
     public MissionStatisticalReport() {
 	regionsList = CommonService.getAllRegions();
@@ -34,6 +47,28 @@ public class MissionStatisticalReport extends BaseBacking {
 	    category = FlagsEnum.ALL.getCode();
 	    regionId = getLoginEmpPhysicalRegionFlag(true);
 	    fromDate = toDate = HijriDateService.getHijriSysDate();
+	    if (reportType == 2) {
+		getInquiryReportPrivileges();
+	    }
+	} catch (BusinessException e) {
+	    this.setServerSideErrorMessages(getMessage(e.getMessage()));
+	}
+    }
+
+    public void getInquiryReportPrivileges() {
+	try {
+	    isAdmin = SecurityService.isEmployeeMenuActionGranted(this.loginEmpData.getEmpId(), MenuCodesEnum.MSN_INQUIRY_REPORT.getCode(), MenuActionsEnum.MSN_INQUIRY_REPORT.getCode());
+	    isManager = loginEmpData.getIsManager().equals(FlagsEnum.ON.getCode());
+
+	    if (isAdmin) {
+		regionId = getLoginEmpPhysicalRegionFlag(isAdmin);
+	    } else if (isManager) {
+		regionId = loginEmpData.getPhysicalRegionId();
+		orgUnitId = loginEmpData.getPhysicalUnitId();
+		orgUnitFullName = loginEmpData.getPhysicalUnitFullName();
+		orgUnitHKey = loginEmpData.getPhysicalUnitHKey();
+	    }
+
 	} catch (BusinessException e) {
 	    this.setServerSideErrorMessages(getMessage(e.getMessage()));
 	}
@@ -41,8 +76,13 @@ public class MissionStatisticalReport extends BaseBacking {
 
     public void print() {
 	try {
-	    byte[] bytes = MissionsService.getMissionStatisicalReport(category, regionId, toDate, fromDate);
-	    super.print(bytes);
+	    if (reportType == FlagsEnum.ON.getCode()) {
+		byte[] bytes = MissionsService.getMissionStatisicalReport(category, regionId, toDate, fromDate);
+		super.print(bytes);
+	    } else if (reportType == 2) {
+		byte[] bytes = MissionsService.getMissionEmployeesStatisicalReport(category, regionId, toDate, fromDate, empId, empFullName, orgUnitId, orgUnitFullName);
+		super.print(bytes);
+	    }
 	} catch (BusinessException e) {
 	    this.setServerSideErrorMessages(getMessage(e.getMessage()));
 	}
@@ -86,6 +126,70 @@ public class MissionStatisticalReport extends BaseBacking {
 
     public void setFromDate(Date fromDate) {
 	this.fromDate = fromDate;
+    }
+
+    public int getReportType() {
+	return reportType;
+    }
+
+    public void setReportType(int reportType) {
+	this.reportType = reportType;
+    }
+
+    public Long getEmpId() {
+	return empId;
+    }
+
+    public void setEmpId(Long empId) {
+	this.empId = empId;
+    }
+
+    public Long getOrgUnitId() {
+	return orgUnitId;
+    }
+
+    public void setOrgUnitId(Long orgUnitId) {
+	this.orgUnitId = orgUnitId;
+    }
+
+    public String getOrgUnitFullName() {
+	return orgUnitFullName;
+    }
+
+    public void setOrgUnitFullName(String unitFullName) {
+	this.orgUnitFullName = unitFullName;
+    }
+
+    public String getEmpFullName() {
+	return empFullName;
+    }
+
+    public void setEmpFullName(String empFullName) {
+	this.empFullName = empFullName;
+    }
+
+    public String getOrgUnitHKey() {
+	return orgUnitHKey;
+    }
+
+    public void setOrgUnitHKey(String orgUnitHKey) {
+	this.orgUnitHKey = orgUnitHKey;
+    }
+
+    public Boolean getIsManager() {
+	return isManager;
+    }
+
+    public void setIsManager(Boolean isManager) {
+	this.isManager = isManager;
+    }
+
+    public Boolean getIsAdmin() {
+	return isAdmin;
+    }
+
+    public void setIsAdmin(Boolean isAdmin) {
+	this.isAdmin = isAdmin;
     }
 
 }
