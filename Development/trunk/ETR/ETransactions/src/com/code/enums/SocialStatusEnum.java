357442fc107f4ac9ae68b655a8e28753a0158259
aabdelgayed <aabdelgayed@c23e29db-d35b-224f-a253-3de55204965d>
2019-01-21 11:15:48 +0000

    adding divorcee and widowed to social status (Related to Issue 0010359 - cr EmployeesDataExtraTransactions)

--- Development/trunk/ETR/ETransactions/src/com/code/enums/SocialStatusEnum.java
+++ Development/trunk/ETR/ETransactions/src/com/code/enums/SocialStatusEnum.java
@@ -2,13 +2,17 @@ package com.code.enums;
 
 public enum SocialStatusEnum {
     SINGLE(1),
-    MARRIED(2);
-    
+    MARRIED(2),
+    DIVORCEE(3),
+    WIDOWED(3);
+
     private int code;
-      
-    private SocialStatusEnum(int code){
-        this.code = code;
+
+    private SocialStatusEnum(int code) {
+	this.code = code;
     }
-    
-    public int getCode(){return code;}
+
+    public int getCode() {
+	return code;
+    }
 }
