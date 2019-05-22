b1dc5fdb3574798cc1ae0e35dc037008ebf2b6f9
khassan <khassan@c23e29db-d35b-224f-a253-3de55204965d>
2019-05-22 11:59:42 +0000

    training plan decision excel

--- src/com/code/ui/backings/hcm/trainings/TrainingPlanDecisionsInquiry.java
+++ src/com/code/ui/backings/hcm/trainings/TrainingPlanDecisionsInquiry.java
@@ -6,10 +6,13 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import com.code.dal.orm.hcm.trainings.TrainingYear;
+import com.code.enums.MenuActionsEnum;
+import com.code.enums.MenuCodesEnum;
 import com.code.enums.ReportOutputFormatsEnum;
 import com.code.exceptions.BusinessException;
 import com.code.services.hcm.TrainingCoursesEventsService;
 import com.code.services.hcm.TrainingSetupService;
+import com.code.services.security.SecurityService;
 import com.code.ui.backings.base.BaseBacking;
 
 @ManagedBean(name = "trainingPlanDecisionsInquiry")
@@ -18,11 +21,14 @@ public class TrainingPlanDecisionsInquiry extends BaseBacking {
 
     private int pageSize = 10;
     private List<TrainingYear> trainingYearsList;
+    private boolean excelPrintFlag = false;
 
     public TrainingPlanDecisionsInquiry() {
 	try {
 	    setScreenTitle(getMessage("title_trainingPlanDecisions"));
 	    trainingYearsList = TrainingSetupService.getApprovedTrainingYears();
+	    if (SecurityService.isEmployeeMenuActionGranted(loginEmpData.getEmpId(), MenuCodesEnum.TRN_PLAN_DECISIONS_INQUIRY.getCode(), MenuActionsEnum.TRN_TRAINING_PLAN_DETAILS_EXCEL_PRINT.getCode()))
+		excelPrintFlag = true;
 	} catch (Exception e) {
 	    e.printStackTrace();
 	}
@@ -46,7 +52,7 @@ public class TrainingPlanDecisionsInquiry extends BaseBacking {
 	    } else if (reportType == 2) {// WORD
 		bytes = TrainingCoursesEventsService.getTrainingPlanDecisionDetailsBytes(trainingYear.getId(), ReportOutputFormatsEnum.RTF.getCode());
 		super.printRTF(bytes);
-	    } else {
+	    } else {// EXCEL
 		bytes = TrainingCoursesEventsService.getTrainingPlanDecisionDetailsBytes(trainingYear.getId(), ReportOutputFormatsEnum.XLS.getCode());
 		super.printXls(bytes);
 	    }
@@ -65,5 +71,9 @@ public class TrainingPlanDecisionsInquiry extends BaseBacking {
 
     public int getPageSize() {
 	return pageSize;
+    }
+
+    public boolean getExcelPrintFlag() {
+	return excelPrintFlag;
     }
 }
