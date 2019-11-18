5e187c89ee1ef063808aad22ed8b49a6b58972ac
aabdelgayed <aabdelgayed@c23e29db-d35b-224f-a253-3de55204965d>
2019-11-18 16:29:52 +0000

    removing not required transient attributes

--- Development/trunk/ETR/ETransactions/src/com/code/dal/orm/workflow/WFProcess.java
+++ Development/trunk/ETR/ETransactions/src/com/code/dal/orm/workflow/WFProcess.java
@@ -9,8 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-import javax.xml.bind.annotation.XmlTransient;
 
 import com.code.dal.orm.BaseEntity;
 
@@ -30,18 +28,13 @@ import com.code.dal.orm.BaseEntity;
 		query = " select p from WFProcess p where p.processId = :P_PROCESS_ID "
 	)
 })
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "WF_PROCESSES")
 public class WFProcess extends BaseEntity implements Serializable {
     private Long processId;
     private String processName;
     private Long processGroupId;
-    
-    private String latinName;
-    private String approveCallback;
-    private String rejectCallback;
-    private String cancelCallback;
-    private Integer enableFlag;
 
     public void setProcessId(Long processId) {
 	this.processId = processId;
@@ -72,56 +65,4 @@ public class WFProcess extends BaseEntity implements Serializable {
     public Long getProcessGroupId() {
 	return processGroupId;
     }
-
-    @Transient
-	@XmlTransient
-	public String getApproveCallback() {
-		return approveCallback;
-	}
-
-	public void setApproveCallback(String approveCallback) {
-		this.approveCallback = approveCallback;
-	}
-
-    @Transient
-	@XmlTransient
-	public String getRejectCallback() {
-		return rejectCallback;
-	}
-
-	public void setRejectCallback(String rejectCallback) {
-		this.rejectCallback = rejectCallback;
-	}
-
-    @Transient
-	@XmlTransient
-	public String getCancelCallback() {
-		return cancelCallback;
-	}
-
-	public void setCancelCallback(String cancelCallback) {
-		this.cancelCallback = cancelCallback;
-	}
-
-    @Transient
-	@XmlTransient
-	public Integer getEnableFlag() {
-		return enableFlag;
-	}
-
-	public void setEnableFlag(Integer enableFlag) {
-		this.enableFlag = enableFlag;
-	}
-
-    @Transient
-	@XmlTransient
-	public String getLatinName() {
-		return latinName;
-	}
-
-	public void setLatinName(String latinName) {
-		this.latinName = latinName;
-	}
-    
-    
 }
