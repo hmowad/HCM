b4afb0c8b3b9421fbd7de48eedbe345a8040c017
ayassin <ayassin@c23e29db-d35b-224f-a253-3de55204965d>
2020-07-09 14:20:47 +0000

    Adding Webservice For Basic Employees Info

---
+++ src/com/code/integration/responses/hcm/WSEmployeeBasicInfoResponse.java
@@ -0,0 +1,128 @@
+package com.code.integration.responses.hcm;
+
+public class WSEmployeeBasicInfoResponse {
+    private String name;
+    private String managerName;
+    private String jobDesc;
+    private String socialID;
+    private String physicalUnitFullName;
+    private String officialMobileNumber;
+    private String directPhoneNumber;
+    private String privateMobileNumber;
+    private String phoneExt;
+    private String shieldMobileNumber;
+    private String ipPhoneExt;
+    private String email;
+
+    public WSEmployeeBasicInfoResponse(String name, String managerName, String jobDesc, String socialID, String physicalUnitFullName, String officialMobileNumber, String directPhoneNumber, String privateMobileNumber, String phoneExt, String shieldMobileNumber, String ipPhoneExt, String email) {
+	this.name = name;
+	this.managerName = managerName;
+	this.jobDesc = jobDesc;
+	this.socialID = socialID;
+	this.physicalUnitFullName = physicalUnitFullName;
+	this.officialMobileNumber = officialMobileNumber;
+	this.directPhoneNumber = directPhoneNumber;
+	this.privateMobileNumber = privateMobileNumber;
+	this.phoneExt = phoneExt;
+	this.shieldMobileNumber = shieldMobileNumber;
+	this.ipPhoneExt = ipPhoneExt;
+	this.email = email;
+    }
+
+    public String getName() {
+	return name;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public String getManagerName() {
+	return managerName;
+    }
+
+    public void setManagerName(String managerName) {
+	this.managerName = managerName;
+    }
+
+    public String getJobDesc() {
+	return jobDesc;
+    }
+
+    public void setJobDesc(String jobDesc) {
+	this.jobDesc = jobDesc;
+    }
+
+    public String getSocialID() {
+	return socialID;
+    }
+
+    public void setSocialID(String socialID) {
+	this.socialID = socialID;
+    }
+
+    public String getPhysicalUnitFullName() {
+	return physicalUnitFullName;
+    }
+
+    public void setPhysicalUnitFullName(String physicalUnitFullName) {
+	this.physicalUnitFullName = physicalUnitFullName;
+    }
+
+    public String getOfficialMobileNumber() {
+	return officialMobileNumber;
+    }
+
+    public void setOfficialMobileNumber(String officialMobileNumber) {
+	this.officialMobileNumber = officialMobileNumber;
+    }
+
+    public String getDirectPhoneNumber() {
+	return directPhoneNumber;
+    }
+
+    public void setDirectPhoneNumber(String directPhoneNumber) {
+	this.directPhoneNumber = directPhoneNumber;
+    }
+
+    public String getPrivateMobileNumber() {
+	return privateMobileNumber;
+    }
+
+    public void setPrivateMobileNumber(String privateMobileNumber) {
+	this.privateMobileNumber = privateMobileNumber;
+    }
+
+    public String getPhoneExt() {
+	return phoneExt;
+    }
+
+    public void setPhoneExt(String phoneExt) {
+	this.phoneExt = phoneExt;
+    }
+
+    public String getShieldMobileNumber() {
+	return shieldMobileNumber;
+    }
+
+    public void setShieldMobileNumber(String shieldMobileNumber) {
+	this.shieldMobileNumber = shieldMobileNumber;
+    }
+
+    public String getIpPhoneExt() {
+	return ipPhoneExt;
+    }
+
+    public void setIpPhoneExt(String ipPhoneExt) {
+	this.ipPhoneExt = ipPhoneExt;
+    }
+
+    public String getEmail() {
+	return email;
+    }
+
+    public void setEmail(String email) {
+	this.email = email;
+    }
+
+}
